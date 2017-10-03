d5653880a292f1e7ba414c10e4155f46b790f0d3
jeffsenk <jeff.senk@gmail.com>
2017-10-03 14:31:13 +0900

    sketch Group create form

--- src/components/NewGroupView.js
+++ src/components/NewGroupView.js
@@ -4,21 +4,40 @@ export default class NewGroupView extends React.Component{
   constructor(props){
     super(props)
     this.handleCancel=this.handleCancel.bind(this);
+    this.state={
+      name:"",
+      parentGroup:""
+    }
+    this.handleChange=this.handleChange.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    const label = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [label]:value
+    });
   }
 
   handleCancel(){
     this.props.resetAfter();
   }
 
+  handleSubmit(){
+    console.log(this.state.name,this.state.parentGroup);
+  }
+
   render(){
     return(
       <div>
-        <div>
-	  New Group
-        </div>
-        <div onClick={this.handleCancel}>
-          Cancel
-        </div>
+        <div>  New Group </div>
+        <div>  Name </div>
+          <input type ="text" name="name" value={this.state.name} onChange={this.handleChange}/>
+        <div>  Parent Group </div>
+          <input type ="text" name="parentGroup" value={this.state.parentGroup} onChange={this.handleChange}/>
+        <div onClick={this.handleCancel}>Cancel</div>
+        <div onClick={this.handleSubmit}>Create</div>
       </div>
     );
   }
